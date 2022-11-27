99fdf8d882254167fb809479913b19d04aa4ee7c
mariusz-dabal <mariusz9dabal@gmail.com>
2022-11-27 19:50:08 +0100

    GYST-16 Add JWT auth

--- gyst/urls.py
+++ gyst/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('api/auth/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
