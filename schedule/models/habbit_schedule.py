6a3b9d01d5b9d2dd6f964dc6df7b0ac02912f888
dobauron <safetysafetystrazak@gmail.com>
2022-12-08 16:12:04 +0100

    gyst21 delete schedule app

--- schedule/models/habbit_schedule.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from schedule.models.habbit_day import DailyHabbit
-from accounts.models import Account
-
-class Schedule(models.Model):
-    day = models.ForeignKey(DailyHabbit, on_delete=models.CASCADE)
-    user = models.OneToOneField(Account, on_delete=models.CASCADE)
