8798e29abadb1f39a9d6680d7f2bb91a964dbcf0
dobauron <safetysafetystrazak@gmail.com>
2022-12-30 12:13:18 +0100

    GYST-30 unittest for accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.urls import reverse
+from .models import Account
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class AuthViewsTests(APITestCase):
+    def setUp(self):
+        self.email = "lemcio@mail.com"
+        self.password = "eldote"
+        self.data = {"email": self.email, "password": self.password}
+        self.url = reverse("token_obtain_pair")
+        self.user = Account.objects.create_user(
+            email="lemcio@mail.com", password="eldote"
+        )
+        self.response = self.client.post(self.url, self.data, format="json")
+        self.token = self.response.data["access"]
+        self.client.credentials(HTTP_AUTHORIZATION="JWT {0}".format(self.token))
+
+    def test_user_is_active(self):
+        self.assertEqual(self.user.is_active, 1, "Active User")
+
+    def test_user_is_not_superuser(self):
+        self.assertEqual(self.user.is_superuser, 0)
+
+    def test_user_is_not_anonymous(self):
+        self.assertEqual(self.user.is_anonymous, 0)
+
+    def test_user_is_not_staff(self):
+        self.assertEqual(self.user.is_staff, 0)
+
+    def test_user_obtain_JWT(self):
+        self.assertEqual(
+            self.response.status_code, status.HTTP_200_OK, self.response.content
+        )
+
+    def test_user_is_autheticated(self):
+        self.assertEqual(self.user.is_authenticated, 1)
+
+    def test_user_is_unauthorized(self):
+        response = self.client.post(
+            self.url, {"email": "user@foo.com", "password": "pass"}, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
