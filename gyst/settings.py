b1718db263cd253d0c25846d2c30125251a17867
Uaciej <amacedmace@gmail.com>
2023-01-12 14:09:45 +0100

    Changed CreateAPIView to ListCreateAPIView and added GET

--- gyst/settings.py
+++ gyst/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
+    "dotenv",
     "accounts",
     "habits",
     "colorfield",
--- habits/views.py
+++ habits/views.py
@@ -2,11 +2,20 @@ from rest_framework import generics
 from .serializers import HabitSerializer
 from .models import Habit
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
-class HabitCreateView(generics.CreateAPIView):
+class HabitCreateView(generics.ListCreateAPIView):
     serializer_class = HabitSerializer
     permission_classes = (IsAuthenticated,)
 
+    def get_queryset(self):
+        # Return the list of habits for the logged-in user
+        return Habit.objects.filter(account=self.request.user)  
+
     def perform_create(self, serializer):
         serializer.save(account=self.request.user)
+
+
+    
