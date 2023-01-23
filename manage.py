6ef64d45a794395cdd561783291834c97279c21e
mariusz-dabal <mariusz9dabal@gmail.com>
2023-01-23 13:04:12 +0100

    GYST-49 fix formatting

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ import dotenv
 def main():
     """Run administrative tasks."""
     dotenv.read_dotenv()
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gyst.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gyst.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -21,5 +21,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
