8787c78e7bc7555b26ec80627cdbe4ceacc8598d
dobauron <safetysafetystrazak@gmail.com>
2023-01-24 17:44:39 +0100

    GYST-39 GET HabitInstance for date from URL

--- habits/urls.py
+++ habits/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HabitListCreateView, HabitRetrieveUpdateDestroyAPIView
+from .views import HabitListCreateView, HabitRetrieveUpdateDestroyAPIView, HabitInstanceListAPIView
 
 urlpatterns = [
     path("", HabitListCreateView.as_view(), name="habits"),
     path("<int:pk>/", HabitRetrieveUpdateDestroyAPIView.as_view(), name="habit_detail"),
+    path("instance/<str:completed_at>/", HabitInstanceListAPIView.as_view(), name="habit_instance_list")
 ]
--- habits/views.py
+++ habits/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from .serializers import HabitSerializer
-from .models import Habit
+from .serializers import HabitSerializer, HabitInstanceSerializer
+from .models import Habit, HabitInstance
 from rest_framework.permissions import IsAuthenticated
 
 
@@ -21,3 +21,14 @@ class HabitRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return Habit.objects.filter(account=self.request.user)
+
+class HabitInstanceListAPIView(generics.ListAPIView):
+    serializer_class = HabitInstanceSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        date_getted_from_url = self.kwargs['completed_at']
+        queryset = HabitInstance.objects\
+            .filter(completed_at=date_getted_from_url)\
+            .filter(habit__account=self.request.user)
+        return queryset
