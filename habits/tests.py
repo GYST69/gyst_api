0ee2474dc11dc2761a5b29fa402f13b40b470e78
dobauron <safetysafetystrazak@gmail.com>
2023-01-29 06:15:47 +0100

    GYST-Unittest, tested by using serialized model

--- habits/tests.py
+++ habits/tests.py
@@ -3,11 +3,11 @@ from django.urls import reverse
 from rest_framework import status
 from .models import Account, Habit
 from django.contrib.auth import get_user_model
+from .serializers import HabitSerializer
 
 
 class TestAPIViews(APITestCase):
     def setUp(self) -> None:
-
         self.user = Account.objects.create_user(
             email="user@email.com", password="1234", is_active=True
         )
@@ -19,20 +19,14 @@ class TestAPIViews(APITestCase):
             "habit_detail", kwargs={"pk": self.habit.id}
         )
         self.client.force_authenticate(user=self.user)
+        self.serializer = HabitSerializer(instance=self.habit)
 
     def test_authenticated_habit_retrieve(self):
-        habit_data = {
-            "id": self.habit.id,
-            "name": "test",
-            "color": "#abcdef",
-            "description": "This is test",
-        }
-
         response = self.client.get(
             self.habit_url_DetailAPIView,
         )
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, habit_data)
+        self.assertEqual(response.data, self.serializer.data)
 
     def test_authenticated_habit_create(self):
         data = {
@@ -41,19 +35,24 @@ class TestAPIViews(APITestCase):
             "color": "#abcfff",
             "description": "This is test for create",
         }
-
         response = self.client.post(
             self.habit_url_ListCreateAPIView,
             data,
         )
+        habit_from_database = Habit.objects.filter(name="test_create").first()
+        habit_from_database_serialized = HabitSerializer(instance=habit_from_database)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, habit_from_database_serialized.data)
 
     def test_authenticated_habit_patch(self):
         data = {"description": "This is test for update"}
-        response = self.client.patch(self.habit_url_DetailAPIView, data)
-        description_field = Habit.objects.filter(name="test").values("description")[0]
+        response = self.client.patch(
+            self.habit_url_DetailAPIView,
+            data)
+        habit_from_database = Habit.objects.filter(name="test").first()
+        habit_from_database_serialized = HabitSerializer(instance=habit_from_database)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(description_field, data)
+        self.assertEqual(response.data, habit_from_database_serialized.data)
 
     def test_authenticated_habit_delete(self):
         response = self.client.delete(
@@ -63,7 +62,7 @@ class TestAPIViews(APITestCase):
         self.assertFalse(Habit.objects.filter(id=self.habit.id).exists())
 
     def test_authenticated_habit_list(self):
-        for habit_number in range(4):
+        for habit_number in range(3):
             Habit.objects.create(
                 account=self.user,
                 name="test " + str(habit_number),
