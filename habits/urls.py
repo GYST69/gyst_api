efe73ed82932a87b63bcf88c46852e078c42c285
dobauron <safetysafetystrazak@gmail.com>
2023-02-02 10:19:07 +0100

    GYST-39 small corrections

--- habits/urls.py
+++ habits/urls.py
@@ -8,9 +8,6 @@ from .views import (
 urlpatterns = [
     path("", HabitListCreateView.as_view(), name="habits"),
     path("<int:pk>/", HabitRetrieveUpdateDestroyAPIView.as_view(), name="habit_detail"),
-    path(
-        "instance/<str:completed_at>/",
-        HabitInstanceListAPIView.as_view(),
-        name="habit_instance_list",
+    path("instance/<str:completed_at>/", HabitInstanceListAPIView.as_view(), name="habit_instance_list",
     ),
 ]
