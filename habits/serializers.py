bb0c14df4da11f73ee058f4ff560eafdbec9140b
dobauron <safetysafetystrazak@gmail.com>
2023-03-10 14:57:34 +0100

    GYST-52 validate for habit level

--- habits/serializers.py
+++ habits/serializers.py
@@ -11,8 +11,7 @@ class HabitSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         root_id = attrs.get("root_id")
         habit_level = attrs.get("habit_level")
-        if root_id is not None and habit_level is not None:
-            # Check if a habit with the same root_id and habit_level already exists
+        if root_id is not None:
             queryset = Habit.objects.filter(root_id=root_id, habit_level=habit_level)
             if self.instance is not None:
                 queryset = queryset.exclude(pk=self.instance.pk)
