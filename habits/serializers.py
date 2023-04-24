f7027e807070b6a53afe3625dfbaa875a3224e57
dobauron <safetysafetystrazak@gmail.com>
2023-04-24 11:44:40 +0200

    GYST-55 blacked, conflict solved

--- habits/serializers.py
+++ habits/serializers.py
@@ -6,13 +6,13 @@ from rest_framework.validators import UniqueTogetherValidator
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
-        fields = ("id", "name", "color", "description", "root_id", "habit_level")
+        fields = ("id", "name", "color", "description", "root", "habit_level")
 
     def validate(self, attrs):
-        root_id = attrs.get("root_id")
+        root_id = attrs.get("root")
         habit_level = attrs.get("habit_level")
         if root_id is not None and habit_level is not None:
-            queryset = Habit.objects.filter(root_id=root_id, habit_level=habit_level)
+            queryset = Habit.objects.filter(root=root, habit_level=habit_level)
             if self.instance is not None:
                 queryset = queryset.exclude(pk=self.instance.pk)
             if queryset.exists():
