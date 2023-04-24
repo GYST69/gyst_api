1ad714420ea17e35ad8de056ef64b3afc69306ab
dobauron <safetysafetystrazak@gmail.com>
2023-04-24 11:52:16 +0200

    GYST-55 blacked, conflict solved new pull

--- habits/models.py
+++ habits/models.py
@@ -3,6 +3,8 @@ from datetime import date
 from accounts.models import Account
 from colorfield.fields import ColorField
 from rest_framework.validators import UniqueValidator
+from colour import Color
+import colorsys
 
 
 class Habit(models.Model):
@@ -17,9 +19,7 @@ class Habit(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     description = models.TextField(max_length=500, blank=True)
-    root_id = models.ForeignKey(
-        "self", null=True, blank=True, on_delete=models.DO_NOTHING
-    )
+    root = models.ForeignKey("self", null=True, blank=True, on_delete=models.DO_NOTHING)
     habit_level = models.CharField(
         max_length=10, choices=LEVEL_CHOOICES, default="moderate"
     )
@@ -29,6 +29,20 @@ class Habit(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        if self.root is not None:
+            root_habit = Habit.objects.get(id=self.root.id)
+            self.color = root_habit.color
+            color_conversion = list(Color(self.color).get_hsl())
+            if Habit.LEVEL_CHOOICES[0][0] == self.habit_level:
+                color_conversion[2] += 0.1
+            elif Habit.LEVEL_CHOOICES[2][0] == self.habit_level:
+                color_conversion[2] -= 0.1
+            self.color = Color(hsl=color_conversion).hex
+        super().save(force_insert, force_update, using, update_fields)
 
 
 class HabitInstance(models.Model):
--- habits/serializers.py
+++ habits/serializers.py
@@ -9,13 +9,13 @@ from rest_framework.validators import UniqueTogetherValidator
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
-        fields = ("id", "name", "color", "description", "root_id", "habit_level")
+        fields = ("id", "name", "color", "description", "root", "habit_level")
 
     def validate(self, attrs):
-        root_id = attrs.get("root_id")
+        root = attrs.get("root_id")
         habit_level = attrs.get("habit_level")
-        if root_id is not None:
-            queryset = Habit.objects.filter(root_id=root_id, habit_level=habit_level)
+        if root is not None:
+            queryset = Habit.objects.filter(root_id=root, habit_level=habit_level)
             if self.instance is not None:
                 queryset = queryset.exclude(pk=self.instance.pk)
             if queryset.exists():
